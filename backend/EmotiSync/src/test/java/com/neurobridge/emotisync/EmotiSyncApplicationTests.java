ff8e15f878876f629d395c383f401a34d97027bd
SebastianAlvarez71 <u20201a301@upc.edu.pe>
2025-09-16 00:12:04 -0500

    Sistomas2

--- backend/EmotiSync/src/test/java/com/neurobridge/emotisync/EmotiSyncApplicationTests.java
+++ backend/EmotiSync/src/test/java/com/neurobridge/emotisync/EmotiSyncApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoSanMiguelApplicationTests {
+class EmotiSyncApplicationTests {
 
 	@Test
 	void contextLoads() {
