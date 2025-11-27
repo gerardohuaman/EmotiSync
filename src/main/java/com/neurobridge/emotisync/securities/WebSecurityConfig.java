757ce56e17c63a52af97e9ff6ce440dc896a3a56
gerardohuaman <u202313990@upc.edu.pe>
2025-11-27 12:10:19 -0500

    quitando comentarios

--- src/main/java/com/neurobridge/emotisync/securities/WebSecurityConfig.java
+++ src/main/java/com/neurobridge/emotisync/securities/WebSecurityConfig.java
@@ -112,20 +112,7 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers("/login").permitAll()
-                        /*
-                        .requestMatchers("/sintomas/**").permitAll()
-                        .requestMatchers("/planesSuscripcion/**").permitAll()
-                        .requestMatchers("/usuarioejercicios/**").permitAll()
-                        //borrar\
-                        .requestMatchers("/usuarios/**").permitAll()
-                        .requestMatchers("/ejercicios/**").permitAll()
-
-                        .requestMatchers("/roles/**").permitAll()
-                        .requestMatchers("/crisis/**").permitAll()
-                        .requestMatchers("/**").permitAll()
-
                         //Para usar swagger
-                         */
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/index.html").permitAll()
