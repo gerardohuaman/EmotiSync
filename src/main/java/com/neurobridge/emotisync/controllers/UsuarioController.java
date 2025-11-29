2c62e0702befd328ce9e644f958aa28a9312e868
Sebastian <sebas_21_zc@outlook.es>
2025-11-29 00:54:19 -0500

    remove rol_solicitado

--- src/main/java/com/neurobridge/emotisync/controllers/UsuarioController.java
+++ src/main/java/com/neurobridge/emotisync/controllers/UsuarioController.java
@@ -167,29 +167,4 @@ public class UsuarioController {
         return ResponseEntity.ok(dtoList);
     }
 
-    @PostMapping("/solicitar-rol")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<String> solicitarCambioRol(@RequestParam SolicitudCambioRolDTO solicitud) {
-        Usuario u = uS.listId(solicitud.getIdUsuario());
-        if(u == null) return ResponseEntity.badRequest().body("Usuario no encontrado");
-
-        if("ESPECIALISTA". equals(solicitud.nuevoRol)) {
-            if(solicitud.datoExtra == null || solicitud.datoExtra.isEmpty()) {
-                return ResponseEntity.badRequest().body("El nro de Colegiatura es obligatoria para especialistas");
-
-            }
-            try {
-                u.setNroColegiatura(Integer.parseInt(solicitud.datoExtra));
-            } catch (NumberFormatException e) {
-                return ResponseEntity.badRequest().body("El nro de Colegiatura debe ser numerico");
-            }
-            u.setRolSolicitado("ESPECIALISTA");
-        }
-        else if ("FAMILIAR".equals(solicitud.nuevoRol)) {
-            u.setRolSolicitado("FAMILIAR");
-        }
-
-        uS.update(u);
-        return ResponseEntity.ok("Solicitud enviada correctamente. Pendiente de aprobacion.");
-    }
 }
